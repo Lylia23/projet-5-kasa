25aac560c17430dd334dcf136c08290553556671
Lylia23 <alinaaline23@gmail.com>
2024-05-05 10:58:52 +0200

    developpement du composant panel et l'integration dans a propos

--- src/pages/a-propos/APropos.jsx
+++ src/pages/a-propos/APropos.jsx
@@ -1,11 +1,17 @@
 import "./aPropos.scss";
 import image from "../../assets/images/p-a-propos.png"
 import {PresentationAccueil} from "../../components/presentation-accueil/PresentationAccueil";
-export function APropos() {
+import {Panel} from "../../components/panel/Panel";
+import {getAPropos} from "../../services/back-end-service";
 
+export function APropos() {
+    const datas = getAPropos();
     return (
         <div>
-           <PresentationAccueil imgPath={image}/>
+            <PresentationAccueil imgPath={image}/>
+            {datas.map((data, index) =>
+                <Panel key={index} titre={data.title} paragraphe={data.content}/>
+            )}
         </div>
     )
 }
